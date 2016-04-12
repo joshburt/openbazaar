3e8aeca9365db5b9dc234b461b47d660ddb72916
Joshua C. Burt <joshburt@shapeandshare.com>
2016-04-11 18:19:57 -0600

    python and pip managed pieces

--- libraries/source_helper.rb
+++ libraries/source_helper.rb
@@ -8,7 +8,38 @@ module OpenBazaar
   module SourceHelper
     def install_source_dependencies
 
+      ###############################################################################
+      ## PIP Managed Dependencies
+      ###############################################################################
+      python_runtime '2'
+      python_package 'cryptography'
+
+      ###############################################################################
+      ## Install Distro Managed / OS Level Packages
+      ###############################################################################
       include_recipe 'build-essential'
+      include_recipe 'openssl::upgrade'
+
+      apt_managed_package_list = %w(
+          libssl-dev
+          libffi-dev
+          libzmq3-dev
+          pkg-config
+          libtool
+          python-dev
+          python-pip
+      )
+      package apt_managed_package_list do
+        action :install
+      end
+
+      ###############################################################################
+      ## Special Handling For libsodium-dev
+      ###############################################################################
+
+
+
+
 
     end
 
