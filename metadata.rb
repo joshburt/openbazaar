66e25fb3baf1f9531b33f28e3e8c4e5b1e0c9647
Joshua C. Burt <joshburt@shapeandshare.com>
2016-04-16 20:56:43 -0600

    metadata update

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description      'Installs/Configures openbazaar'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 
+issues_url 'https://github.com/joshburt/openbazaar/issues'
+source_url 'https://github.com/joshburt/openbazaar'
+
 depends 'git', '~> 4.0'
 depends 'openssl'
 depends 'chef-sugar'
