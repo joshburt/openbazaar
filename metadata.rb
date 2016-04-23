6dcadd1b2d9bc0304ae3adfe3b8d52c4447e973c
Joshua C. Burt <joshburt@shapeandshare.com>
2016-04-23 15:37:35 -0600

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'joshburt@shapeandshare.com'
 license          'MIT License'
 description      'Installs/Configures openbazaar'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.3'
+version          '0.3.4'
 
 issues_url 'https://github.com/joshburt/openbazaar/issues'
 source_url 'https://github.com/joshburt/openbazaar'
